f30cd69c3ddd493da28b8ccecb358aeb9ec234a3
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-01-23 23:32:14 -0500

    Added 3 enemy classes
    -Enemy class is now Goblin class
    -Added Troll and Vampire classes
    -When an enemy spawns one of the three classes is now chosen at random to be spawned

--- enemies.js
+++ enemies.js
@@ -4,13 +4,16 @@ const enemies = [];				// array of existing enemies
 const enemyPositions = [];		// array of enemy vertical/row positions
 
 // Enemies that advance on towers
-class Enemy{
+
+//Goblin Enemy
+class Goblin{
 	constructor(verticalPosition){
 		this.x = canvas.width;
 		this.y = verticalPosition;
 		this.width = cellSize - cellGap * 2;
 		this.height = cellSize - cellGap * 2;
-		this.speed = Math.random()*0.2+0.4;
+		//this.speed = Math.random()*0.2+0.4;
+		this.speed = 0.4;
 		this.movement = this.speed;
 		this.health = 100;
 		this.maxHealth = this.health; //will award player resources based on how large the HP was
@@ -21,9 +24,67 @@ class Enemy{
 	}
 	// Draw the enemy
 	draw(){
+		context.fillStyle = 'green';
+		context.fillRect(this.x, this.y, this.width, this.height);
+		context.fillStyle = 'brown';
+		context.font = '30px Orbitron';
+		context.fillText(Math.floor(this.health),this.x + 15, this.y + 30);
+	}
+}
+
+//Vampire Enemy
+class Vampire{
+	constructor(verticalPosition){
+		this.x = canvas.width;
+		this.y = verticalPosition;
+		this.width = cellSize - cellGap * 2;
+		this.height = cellSize - cellGap * 2;
+		//this.speed = Math.random()*0.2+0.4;
+		this.speed = 1.2;
+		this.movement = this.speed;
+		this.health = 60;
+		this.maxHealth = this.health; //will award player resources based on how large the HP was
+	}
+	// Move the enemy
+	update(){
+		this.x -= this.movement; //walking right to left
+
+		if (this.health < this.maxHealth){     //vampire's regain health over time
+			this.health += 0.1;
+		}
+	}
+
+	draw(){
 		context.fillStyle = 'red';
 		context.fillRect(this.x, this.y, this.width, this.height);
-		context.fillStyle = 'blue';
+		context.fillStyle = 'grey';
+		context.font = '30px Orbitron';
+		context.fillText(Math.floor(this.health),this.x + 15, this.y + 30);
+	}
+}
+
+//Troll Enemy
+class Troll{
+	constructor(verticalPosition){
+		this.x = canvas.width;
+		this.y = verticalPosition;
+		this.width = cellSize - cellGap * 2;
+		this.height = cellSize - cellGap * 2;
+		//this.speed = Math.random()*0.2+0.4;
+		this.speed = 0.2;
+		this.movement = this.speed;
+		this.health = 400;
+		this.maxHealth = this.health; //will award player resources based on how large the HP was
+	}
+	// Move the enemy
+	update(){
+		this.x -= this.movement; //walking right to left
+	}
+
+	draw(){
+		context.fillStyle = 'purple';
+		context.fillRect(this.x, this.y, this.width, this.height);
+		context.fillStyle = 'green';
 		context.font = '30px Orbitron';
 		context.fillText(Math.floor(this.health),this.x + 15, this.y + 30);
 	}
@@ -59,8 +120,19 @@ function spawnNewEnemies(){
 	if (frame% enemiesInterval === 0 && score < winningScore){ 
 		// Determine row position
 		let verticalPosition = Math.floor(Math.random()*5 +1) * cellSize + cellGap;
-		// Add enemy to enemies array
-		enemies.push(new Enemy(verticalPosition));
+		
+		// Add random enemy to enemies array
+		let enemySelector = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+		if (enemySelector == 1){
+			enemies.push(new Goblin(verticalPosition));
+		}
+		else if (enemySelector == 2) {
+			enemies.push(new Vampire(verticalPosition));
+		}
+		else{
+			enemies.push(new Troll(verticalPosition));
+		}
+		
 		// Add enemy row position to the array
 		enemyPositions.push(verticalPosition);
 		// Speed up rate that enemies appear.
