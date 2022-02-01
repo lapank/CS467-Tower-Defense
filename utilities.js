d471670b4bc8904b1001b7627b20727cc1307b45
justindavidtodd <toddjus@oregonstate.edu>
2022-02-01 15:45:26 -0700

    Added Comment describing how to set 'select' variable to apply to different screens

--- utilities.js
+++ utilities.js
@@ -1,7 +1,7 @@
 let gameOver = false;		// When True, game is lost.
 let victory = false;
 let running;				// When true, gameloop runs.
-let select = 0;				// Controls which screen to open
+let select = 0;				// Controls which screen to open 0:Title, -1:LevelSelect, 1:GameRunning
 const winningScore = 100;	// Points needed to win
 
 const title = {
