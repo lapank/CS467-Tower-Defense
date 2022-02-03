84b2f84c632af37d7b62ae8d2c5343f8cff011ea
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-02 23:22:55 -0500

    added sprite

--- enemies/troll.js
+++ enemies/troll.js
@@ -1,9 +1,26 @@
+//Troll Enemy
+const trollImage = new Image();
+trollImage.src = 'sprites/troll.png';
+
 class Troll extends Enemy{
 	constructor(verticalPosition){
 		super(verticalPosition, 0.2, 400);
+
+		//info for applying sprite sheet
+		this.sprite = trollImage;
+		this.frameX = 0;
+		this.frameY = 0; 
+		this.minFrame = 0;
+		this.maxFrame = 9;
+		this.spriteWidth = 1602;
+		this.spriteHeight = 1002;
 	}
 	draw(){
 		super.draw('purple','blue');
+
+		// Draw the sprite
+		context.drawImage(this.sprite, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x - 80, this.y - 70, this.width*3.6, this.height*2.);
+
 	}
 
 }
