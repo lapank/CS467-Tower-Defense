7be104fc7d2b238bb83df556517c05e12c68c296
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-06 20:16:30 -0500

    dragon sprite

--- towers/dragon.js
+++ towers/dragon.js
@@ -1,11 +1,30 @@
+const dragonImage = new Image();
+dragonImage.src = 'sprites/dragon.png';
+
 class Dragon extends Tower{
 	constructor(x,y){
 		super(x,y, 150, 0, 200);
+
+		//info for applying sprite sheet
+		this.sprite = dragonImage;
+		this.frameX = 0;
+		this.frameY = 0; 
+		this.minFrame = 0;
+		this.maxFrame = 1;
+		this.spriteWidth = 82;
+		this.spriteHeight = 82;
 	}
 	draw(){
+		// Draw the sprite
+		context.drawImage(this.sprite, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x - 10, this.y + 15, this.width*1.1, this.height*1.1);
 		super.draw('lime', 'black');
 	}
 	update(){
 		super.update(FireBall);
+		// Cycle through sprite
+		if (frame % 25  === 0){
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = this.minFrame;
+        }
 	}
 }
