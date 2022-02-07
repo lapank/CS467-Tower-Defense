96587bb2184b352f330161b263785725ba0ba967
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-06 20:26:47 -0500

    functionality for projectile sprites
    also removed drawing of circles for projectiles

--- projectiles/projectiles.js
+++ projectiles/projectiles.js
@@ -12,12 +12,19 @@ class Projectile {
 	// Move the projectile
 	update(){
 		this.x += this.speed;
+
+		// Cycle through sprite
+		if (frame % 25  === 0){
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = this.minFrame;
+        }
 	}
 	// Draw the projectile
 	draw(color){
-		context.fillStyle = color;
+		/*context.fillStyle = color;
 		context.beginPath();
 		context.arc(this.x,this.y, this.width, 0, Math.PI * 2);
 		context.fill();
+		*/
 	}
 }
