386b4f21350ae3130a25225ca8a1b9d3f95d96c8
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-06 20:17:21 -0500

    added wizard sprite

--- towers/wizard.js
+++ towers/wizard.js
@@ -1,11 +1,30 @@
+const wizardImage = new Image();
+wizardImage.src = 'sprites/wizard.png';
+
 class Wizard extends Tower{
 	constructor(x,y){
 		super(x,y, 75, 0, 40);
+
+		//info for applying sprite sheet
+		this.sprite = wizardImage;
+		this.frameX = 0;
+		this.frameY = 0; 
+		this.minFrame = 0;
+		this.maxFrame = 1;
+		this.spriteWidth = 66;
+		this.spriteHeight = 66;
 	}
 	draw(){
+		// Draw the sprite
+		context.drawImage(this.sprite, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x - 10, this.y + 15, this.width*1.1, this.height*1.1);
 		super.draw('skyblue', 'gold');
 	}
 	update(){
 		super.update(MagicBolt);
+		// Cycle through sprite
+		if (frame % 10  === 0){
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = this.minFrame;
+        }
 	}
 }
