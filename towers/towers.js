94594144ca4ae6dc77c704f89b1395fdb19e0389
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-08 21:10:15 -0500

    Update towers.js
    Restored original update() function, moved functionality from update() to subclass files

--- towers/towers.js
+++ towers/towers.js
@@ -13,12 +13,12 @@ class Tower{
 		this.shooting = false;
 		this.health = health;
 		this.timer = timer;
-		this.fireSpeed = fireSpeed;  // number of timer-frames between shots.
+		this.fireSpeed = fireSpeed;  // time between animation frames when shooting
 		this.towerSelector = towerSelector;
 	}
 	// Draw Tower on board
 	draw(bodyColor, textColor){
-		//context.fillStyle = bodyColor;
+		//context.fillStyle = bodyColor; 
 		//context.fillRect(this.x,this.y,this.width,this.height);
 		context.fillStyle = textColor;
 		context.font = '30px Orbitron';
@@ -27,64 +27,15 @@ class Tower{
 	// Take a projectile class. Increment timer and fire projectile.
 	update(projectile){
 		if(this.shooting){
-			
-			if (this.sprite == dragonImage){
-				//attack animation frames
-				this.minFrame = 0;
-				this.maxFrame = 7;
-
-				//fire projectile on firing frame
-				if( this.frameX == 7){
-					projectiles.push(new projectile(this.x + 70, this.y + 50));
-					this.frameX = 0;
-				}
+			this.timer++;
+			if(this.timer % this.fireSpeed === 0){
+				projectiles.push(new projectile(this.x + 70, this.y + 50));
 			}
-			else if(this.sprite == wizardImage){
-				//attack animation frames
-				this.minFrame = 0;
-				this.maxFrame = 5;
-
-				//fire projectile on firing frame
-				if( this.frameX == 5){
-					projectiles.push(new projectile(this.x + 70, this.y + 50));
-					this.frameX = 0;
-				}
-			}
-			else if(this.sprite == archerImage){
-				//attack animation frames
-				this.minFrame = 4;
-				this.maxFrame = 7;
-
-				//fire projectile on firing frame
-				if( this.frameX == 7){
-					projectiles.push(new projectile(this.x + 70, this.y + 80));
-					this.frameX = this.minFrame;
-				}
-			}
-			
-
-			
-
-	
 		}
 		// Reset timer if not shooting
 		else{ 
-				//return to idle animation
-				if (this.sprite == dragonImage){
-					this.minFrame = 0;
-					this.maxFrame = 1;
-				}
-				else if (this.sprite == wizardImage){
-					this.minFrame = 0;
-					this.maxFrame = 1;
-				}
-				else if (this.sprite == archerImage){
-					this.minFrame = 0;
-					this.maxFrame = 1;
-				}	
-			}
-
-		
+			this.timer = 0;
+		}
 	}
 }
 
