023bdf8cb60f3f3ce003be24413dfe95c202d725
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-08 21:13:10 -0500

    Update wizard.js
    Sprite animation moved to this update() function. Added animationRate and idleRate.
    fireSpeed now used in determining frame rate while attacking.

--- towers/wizard.js
+++ towers/wizard.js
@@ -3,16 +3,18 @@ wizardImage.src = 'sprites/wizard.png';
 
 class Wizard extends Tower{
 	constructor(x,y){
-		super(x,y, 75, 0, 40);
+		super(x,y, 75, 0, 15);
 
 		//info for applying sprite sheet
 		this.sprite = wizardImage;
 		this.frameX = 0;
 		this.frameY = 0; 
 		this.minFrame = 0;
-		this.maxFrame = 1;
+		this.maxFrame = 5;
 		this.spriteWidth = 66;
 		this.spriteHeight = 66;
+		this.idleRate = 10; // time between animation frames while idle
+		this.animationRate = this.idleRate; // time between animation frames
 	}
 	draw(){
 		// Draw the sprite
@@ -20,11 +22,30 @@ class Wizard extends Tower{
 		super.draw('skyblue', 'gold');
 	}
 	update(){
-		super.update(MagicBolt);
-		// Cycle through sprite
-		if (frame % 10  === 0){
+		// Cycle through sprite sheet 
+		if (frame % this.animationRate === 0){
             if (this.frameX < this.maxFrame) this.frameX++;
             else this.frameX = this.minFrame;
         }
-	}
+
+		// Cycle through attack animation
+		if(this.shooting){
+			//attack animation frames
+			this.minFrame = 5;
+			this.maxFrame = 9;
+			this.animationRate = this.fireSpeed; //attack animation rate
+
+			//fire projectile on firing frame
+			if( this.frameX == this.maxFrame){
+				projectiles.push(new MagicBolt(this.x + 45, this.y + 45));
+				this.frameX = this.minFrame;
+			}
+		}
+		// Return to idle animation when not shooting
+		else{ 
+				this.minFrame = 0;
+				this.maxFrame = 5;
+				this.animationRate = this.idleRate; //idle animation rate
+			}
+		}
 }
