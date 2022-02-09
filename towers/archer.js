368bd66ae39af74c6bd37dab9533f717ebe13b37
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-08 21:12:03 -0500

    Update archer.js
    Sprite animation moved to this update() function. Added animationRate and idleRate.
    fireSpeed now used in determining frame rate while attacking.

--- towers/archer.js
+++ towers/archer.js
@@ -3,7 +3,7 @@ archerImage.src = 'sprites/archer.png';
 
 class Archer extends Tower{
 	constructor(x,y){
-		super(x,y, 100, 0, 100);
+		super(x,y, 100, 0, 20);
 
 		//info for applying sprite sheet
 		this.sprite = archerImage;
@@ -13,17 +13,38 @@ class Archer extends Tower{
 		this.maxFrame = 1;
 		this.spriteWidth = 31.8;
 		this.spriteHeight = 34;
+		this.idleRate = 20; // time between animation frames while idle
+		this.animationRate = this.idleRate; // time between animation frames
 	}
 	draw(){
 		context.drawImage(this.sprite, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x +25, this.y + 55, this.width * 0.35, this.height *0.45);
 		super.draw('saddlebrown', 'white');
 	}
 	update(){
-		super.update(Arrow);
-		// Cycle through sprite
-		if (frame % 20  === 0){
+		// Cycle through sprite 
+		if (frame % this.animationRate  === 0){
             if (this.frameX < this.maxFrame) this.frameX++;
             else this.frameX = this.minFrame;
         }
+
+		// Cycle through attack animation
+		if(this.shooting){
+				//attack animation frames
+				this.minFrame = 4;
+				this.maxFrame = 7;
+				this.animationRate = this.fireSpeed;  //attack animation rate
+
+				//fire projectile on firing frame
+				if( this.frameX == this.maxFrame){
+					projectiles.push(new Arrow(this.x + 50, this.y + 80));
+					this.frameX = this.minFrame;
+				}
+		}
+		// Return to idle animation when not shooting
+		else{ 
+				this.minFrame = 0;
+				this.maxFrame = 1;
+				this.animationRate = this.idleRate; //idle animation rate
+			}
 	}
 }
