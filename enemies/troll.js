5afd19c080404fd8e3c4d401c724d2c890d517d5
mazzolaa <59713660+mazzolaa@users.noreply.github.com>
2022-02-16 21:50:07 -0500

    Update troll.js

--- enemies/troll.js
+++ enemies/troll.js
@@ -2,6 +2,9 @@
 const trollImage = new Image();
 trollImage.src = 'sprites/troll.png';
 
+const trollDeath = new Image();
+trollDeath.src = 'sprites/trollDeath.png'
+
 class Troll extends Enemy{
 	constructor(verticalPosition, delay){
 		super(verticalPosition, 0.2, 400, delay);
@@ -15,6 +18,29 @@ class Troll extends Enemy{
 		this.spriteWidth = 1602;
 		this.spriteHeight = 1002;
 	}
+
+	update(){
+		super.update();
+
+		// Cycle through death animation
+		if(this.dying){
+			this.minFrame = 0;
+			this.maxFrame = 8;
+
+			// Switch to death animation sprite sheet
+			if (this.sprite == trollImage){
+				this.frameX = this.minFrame;
+				this.sprite = trollDeath;
+			}
+
+			//Set dead to true when end of death animation is reached
+			if( this.frameX == this.maxFrame){
+				this.dead = true;
+			}
+			
+		}
+	}
+
 	draw(){
 		// Draw the sprite
 		context.drawImage(this.sprite, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x - 80, this.y - 70, this.width*3.6, this.height*2);
