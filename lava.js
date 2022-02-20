4d1d0a8a94dcf758dd6557c89335fb935159c3be
Kaylin Lapan <kaylinlapan@gmail.com>
2022-02-20 00:05:14 -0500

    Lava falls from sky and disapears after hitting board destination.

--- lava.js
+++ lava.js
@@ -1,15 +1,20 @@
 lavaInterval = 600; 	
-const lava = [];
+const lavaTarget = [];
+const lavaBall = [];
 const lavaPosition = [];
 
 class Lava{
 	constructor(horizontalPosition){
 		this.x = Math.random() * (canvas.width - cellSize);
-		this.y = (Math.floor(Math.random() *5)+1) *cellSize+25;
+		this.y = 100;
+		this.targetY = (Math.floor(Math.random() *5)+1) *cellSize+25;
 		this.width = cellSize - cellGap * 2;
 		this.height = cellSize - cellGap * 2;
 		this.speed = 0.4;
 		this.movement = this.speed;
+	}
+	update(){
+		this.y += this.movement; //walking right to left
 	}
 	draw(){
 		context.fillStyle = 'rgba(255, 0, 0, 0.5)';
@@ -18,8 +23,18 @@ class Lava{
 }
 
 function updateLava(){
-	for( let i = 0; i < lava.length; i++){
-		lava[i].draw();
+	for( let i = 0; i < lavaTarget.length; i++){
+		lavaTarget[i].update();
+		lavaTarget[i].draw();
+		console.log(i + ' ' + lavaTarget[i].y);
+		console.log(i + ' ' + lavaTarget[i].targetY);
+			if (lavaTarget[i].y > lavaTarget[i].targetY){
+			// Remove lava row position from the array 
+			const findThisIndex = lavaPosition.indexOf(lavaTarget[i].x);
+			lavaPosition.splice(findThisIndex, 1);
+			lavaTarget.splice(i, 1);
+			i--;
+		}
 	}
 	spawnLava();
 }
@@ -30,14 +45,13 @@ function spawnLava(){
 		// Determine column position
 		let horizontalPosition = Math.floor(Math.random()*9 +1) * cellSize + cellGap;
 		// Add random lava to lava array
-			lava.push(new Lava(horizontalPosition));
-			console.log("lava array");
-			console.log(lava);
+			lavaBall.push(new FireBall(this.x + 55, this.y + 40));
+			lavaTarget.push(new Lava(horizontalPosition));
+
 	
 		// Add enemy row position to the array
 		lavaPosition.push(horizontalPosition);
-		console.log("lavaPosition array");
-		console.log(lavaPosition);
+
 		// Speed up rate that enemies appear.
 		if (lavaInterval > 120) lavaInterval -= 100;
 	}
