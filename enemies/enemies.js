e57b2761f64e660c9d6ab75ef77ba047957a61a9
justindavidtodd <toddjus@oregonstate.edu>
2022-02-25 14:11:09 -0700

    Enemy health now only appears when they have taken damage.

--- enemies/enemies.js
+++ enemies/enemies.js
@@ -44,7 +44,9 @@ class Enemy{
 		//context.fillRect(this.x, this.y, this.width, this.height);
 		//context.fillStyle = textColor;
 		//context.font = '20px Orbitron';
-		strokedText(Math.floor(this.health).toString(),this.x + 15, this.y + 30, '20px', textColor);
+		if (this.health < this.maxHealth){
+			strokedText(Math.floor(this.health).toString(),this.x + 15, this.y + 30, '20px', textColor);
+		}
 		//context.fillText(Math.floor(this.health),this.x + 15, this.y + 30);
 
 		// Display fire status when on fire
